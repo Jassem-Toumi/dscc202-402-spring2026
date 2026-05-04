7d6bbd70642446aa762161a45a2eb6b352851ed5
Jassem-Toumi <toumijassem@gmail.com>
2026-05-04 02:23:36 +0000

    changed to s3 test

--- final_project/tweet-pipeline/transformations/bronze_tweet_ingest.py
+++ final_project/tweet-pipeline/transformations/bronze_tweet_ingest.py
@@ -109,7 +109,7 @@ def ingest_raw_tweets():
             .option("cloudFiles.format", "json")
             .option("cloudFiles.schemaLocation", "/Volumes/workspace/default/checkpoints/")
             .schema(tweet_schema)
-            .load("s3://dsas-datasets/tweets/")
+            .load("s3://dsas-datasets/test-tweets/")
             .withColumn("source_file",      col("_metadata.file_path"))
             .withColumn("processing_time",  current_timestamp())
     )
